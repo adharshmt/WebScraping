e538868a30fed21ed39fb1e4464da4ded56d29d8
adharshmt <89651250+adharshmt@users.noreply.github.com>
2021-09-05 21:32:18 +0530

    collected image link and text
    collected link for image, text to be printed and link to the news

--- verge.py
+++ verge.py
@@ -11,13 +11,21 @@ driver.get("https://www.theverge.com/")
 sleep(2)
 
 
-contents = driver.find_elements_by_class_name('c-entry-box--compact__title')
-for content in contents:
-    elem = content.find_element_by_tag_name('a')
-    href = elem.get_attribute('href')
-    print(elem.text)
-    print(href)
-    print()
+main_seven = driver.find_elements_by_class_name('c-seven-up__main')
+for main_news in main_seven:
+    single_news = main_news.find_elements_by_class_name('c-entry-box--compact')
+    for news in single_news:
+        picturetag = news.find_element_by_tag_name('picture')
+            picturesource = picturetag.find_element_by_tag_name('source')
+                picture = picturesource.get_attribute('srcset')
+                print(picture.split(" ",1)[0])
+                print('\n')
+        singlenewsfull = news.find_element_by_class_name('c-entry-box--compact__title')
+            taginsingle = singlenewsfull.find_element_by_tag_name('a')
+                href = taginsingle.get_attribute('href')
+                print(taginsingle.text)
+                print('\n')
+                print(href)
 
 
 driver.close()
