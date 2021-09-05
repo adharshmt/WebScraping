1e40c2b2b819ad0b24923cf5a9d1a8b2e8d6c5e3
adharshmt <89651250+adharshmt@users.noreply.github.com>
2021-09-05 11:20:52 +0530

    Update verge.py

--- verge.py
+++ verge.py
@@ -11,42 +11,13 @@ driver.get("https://www.theverge.com/")
 sleep(2)
 
 
-
-
-# contents = driver.find_elements_by_class_name('c-entry-box--compact c-entry-box--compact--article c-entry-box--compact--hero')
-# for content in contents:
-#     href =content.get_attribute('href')
-#     print(href)
-
-
-contents = driver.find_elements_by_class_name('c-entry-box--compact__image-wrapper')
+contents = driver.find_elements_by_class_name('c-entry-box--compact__title')
 for content in contents:
-    href =content.get_attribute('href')
+    elem = content.find_element_by_tag_name('a')
+    href = elem.get_attribute('href')
+    print(elem.text)
     print(href)
+    print()
 
 
-# links = driver.find_elements_by_tag_name('h2')
-# for link in links:
-#     print (link)
-
-
-
-# div = driver.find_element_by_class_name('c-entry-box--compact__title')
-# links = div.find_element_by_css_selector('a').get_attribute('href')
-# print (links)
-
-
-
-# for link in links:      
-#     print(link)
-# contents = driver.find_elements_by_class_name('c-seven-up')
-# for content in contents:
-#     href =content.get_attribute('href')
-#     print(href)
-
-# elems = driver.find_elements_by_tag_name('a')
-# for elem in elems:
-#     href = elem.get_attribute('href')
-#     if href is not None:
-#         print(href)
 driver.close()
